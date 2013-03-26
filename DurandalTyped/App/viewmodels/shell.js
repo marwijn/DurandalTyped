e1a3a68203246f464492b6a2aa3ac134e071c40c
Marwijn <marwijn@hotmail.com>
2013-03-26 18:22:33 +0100

    Changed shell to typescript

--- DurandalTyped/App/viewmodels/shell.js
+++ DurandalTyped/App/viewmodels/shell.js
@@ -1,14 +1,21 @@
-﻿define(['durandal/plugins/router', 'durandal/app'], function (router, app) {
-
-    return {
-        router: router,
-        search: function() {
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-            app.showMessage('Search not yet implemented...');
-        },
-        activate: function () {
-            return router.activate('welcome');
-        }
+var Shell = (function () {
+    function Shell(router, app) {
+        this.app = app;
+        this.router = router;
+    }
+    Shell.prototype.search = function () {
+        this.app.showMessage('Search not yet implemented...');
     };
+    Shell.prototype.activate = function () {
+        return this.router.activate('welcome');
+    };
+    return Shell;
+})();
+define([
+    'durandal/plugins/router', 
+    'durandal/app', 
+    'durandal/system'
+], function (router, app, system) {
+    return new Shell(router, app);
 });
+//@ sourceMappingURL=shell.js.map
---
+++ DurandalTyped/App/viewmodels/shell.ts
@@ -0,0 +1,26 @@
+///<reference path='../../Scripts/typings/requirejs/require.d.ts'/>
+///<reference path='../../Scripts/typings/durandal/durandal.d.ts'/>
+
+
+class Shell {
+    app: any;
+    router: any;
+
+    constructor(router: any, app: App) {
+        this.app = app;
+        this.router = router;
+    }
+
+    public search(): void {
+        this.app.showMessage('Search not yet implemented...');
+    }
+
+    public activate(): any {
+        return this.router.activate('welcome');
+    }
+}
+
+
+define(['durandal/plugins/router', 'durandal/app', 'durandal/system'], function (router, app: App, system) {
+    return new Shell(router, app);
+});
