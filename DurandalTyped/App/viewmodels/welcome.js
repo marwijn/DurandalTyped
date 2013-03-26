3a0c2513dc675d52c037ac0102ae4448e18d8ece
Marwijn <marwijn@hotmail.com>
2013-03-26 18:34:53 +0100

    Changed welcome to typescript

--- DurandalTyped/App/viewmodels/welcome.js
+++ DurandalTyped/App/viewmodels/welcome.js
@@ -1,25 +1,26 @@
-﻿define(function() {
-    var welcome = function () {
+var Welcome = (function () {
+    function Welcome() {
         this.displayName = 'Welcome to the Durandal Starter Kit!';
         this.description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
         this.features = [
-            'Clean MV* Architecture',
-            'JS & HTML Modularity',
-            'Simple App Lifecycle',
-            'Eventing, Modals, Message Boxes, etc.',
-            'Navigation & Screen State Management',
-            'Consistent Async Programming w/ Promises',
-            'App Bundling and Optimization',
-            'Use any Backend Technology',
-            'Built on top of jQuery, Knockout & RequireJS',
-            'Integrates with other libraries such as SammyJS & Bootstrap',
+            'Clean MV* Architecture', 
+            'JS & HTML Modularity', 
+            'Simple App Lifecycle', 
+            'Eventing, Modals, Message Boxes, etc.', 
+            'Navigation & Screen State Management', 
+            'Consistent Async Programming w/ Promises', 
+            'App Bundling and Optimization', 
+            'Use any Backend Technology', 
+            'Built on top of jQuery, Knockout & RequireJS', 
+            'Integrates with other libraries such as SammyJS & Bootstrap', 
             'Make jQuery & Bootstrap widgets templatable and bindable (or build your own widgets).'
         ];
+    }
+    Welcome.prototype.viewAttached = function (view) {
     };
-
-    welcome.prototype.viewAttached = function (view) {
-        //you can get the view after it's bound and connected to it's parent dom node if you want
-    };
-
-    return welcome;
+    return Welcome;
+})();
+define([], function () {
+    return new Welcome();
 });
+//@ sourceMappingURL=welcome.js.map
---
+++ DurandalTyped/App/viewmodels/welcome.ts
@@ -0,0 +1,28 @@
+///<reference path='../../Scripts/typings/requirejs/require.d.ts'/>
+
+class Welcome {
+    displayName = 'Welcome to the Durandal Starter Kit!';
+    description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
+    features = [
+        'Clean MV* Architecture',
+        'JS & HTML Modularity',
+        'Simple App Lifecycle',
+        'Eventing, Modals, Message Boxes, etc.',
+        'Navigation & Screen State Management',
+        'Consistent Async Programming w/ Promises',
+        'App Bundling and Optimization',
+        'Use any Backend Technology',
+        'Built on top of jQuery, Knockout & RequireJS',
+        'Integrates with other libraries such as SammyJS & Bootstrap',
+        'Make jQuery & Bootstrap widgets templatable and bindable (or build your own widgets).'
+    ];
+
+    viewAttached(view: any) {
+        //you can get the view after it's bound and connected to it's parent dom node if you want
+    }
+}
+
+define([], function () {
+    return new Welcome();
+});
+
