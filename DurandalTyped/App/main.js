9d113118e0e5836090035c82a6976752a0ec752a
Marwijn <marwijn@hotmail.com>
2013-03-28 15:53:14 +0100

    Changed Flickr to typescript

--- DurandalTyped/App/main.js
+++ DurandalTyped/App/main.js
@@ -1,4 +1,38 @@
-﻿requirejs.config({
+﻿// Polyfill for Function.bind(). Slightly modified version of
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
+if (typeof Function.prototype.bind !== "function") {
+    Function.prototype.bind = function (oThis) {
+        if (typeof this !== "function") {
+            // closest thing possible to the ECMAScript 5 internal IsCallable function
+            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+        }
+
+        var aArgs =  Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP = function () { },
+            fBound = function () {
+                return fToBind.apply(this instanceof fNOP && oThis ? this : oThis, aArgs.concat());
+            };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+    };
+}
+
+function Rebind(obj) {
+    var prototype = obj.constructor.prototype;
+    for (var name in prototype) {
+        if (!obj.hasOwnProperty(name)
+                && typeof prototype[name] === "function") {
+            var method = prototype[name];
+            obj[name] = method.bind(obj);
+        }
+    }
+}
+
+requirejs.config({
     paths: {
         'text': 'durandal/amd/text'
     }
---
+++ DurandalTyped/App/viewmodels/flickr.ts
@@ -0,0 +1,46 @@
+///<reference path='../../Scripts/typings/requirejs/require.d.ts'/>
+///<reference path='../../Scripts/typings/durandal/durandal.d.ts'/>
+///<reference path='../../Scripts/typings/knockout/knockout.d.ts'/>
+
+
+class Flickr {
+    app: App;
+    http: Http;
+
+    displayName = 'Flickr';
+    images = ko.observableArray([]);
+
+    constructor(app: App, http: Http) {
+        this.app = app;
+        this.http = http;
+    }
+
+    public activate(): any {
+        //the router's activator calls this function and waits for it to complete before proceding
+        if (this.images().length > 0) {
+            return;
+        }
+
+        return this.http.jsonp('http://api.flickr.com/services/feeds/photos_public.gne', { tags: 'mount ranier', tagmode: 'any', format: 'json' }, 'jsoncallback').then((response) =>
+        {
+            this.images(response.items);
+        });
+    }
+    public select(item: any) {
+        //the app model allows easy display of modal dialogs by passing a view model
+        //views are usually located by convention, but you an specify it as well with viewUrl
+        item.viewUrl = 'views/detail';
+        this.app.showModal(item);
+    }
+    public canDeactivate(): any {
+        //the router's activator calls this function to see if it can leave the screen
+        return this.app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
+    }
+}
+
+
+define(['durandal/http', 'durandal/app', 'durandalts/rebind'], function (http, app, rebind) {
+    var flickr = new Flickr(app, http);
+    rebind(flickr);
+    return flickr;
+});
